7fbf2b5290265676cd54760246c774532aa6ee71
shimly <575801327@qq.com>
2016-05-27 20:27:19 +0800

    no message

---
+++ StoryBoardDemo2/StoryBoardDemo2/CustomTabarController.swift
@@ -0,0 +1,90 @@
+//
+//  CustomTabarController.swift
+//  StoryBoardDemo2
+//
+//  Created by shimly on 16/5/27.
+//  Copyright © 2016年 shimly. All rights reserved.
+//
+
+import UIKit
+
+class CustomTabarController: UITabBarController {
+
+    
+    var selectButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+      let rect =   self.tabBar.frame;
+        
+    
+        
+        self.tabBar.removeFromSuperview();
+        let customView = UIView(frame: rect);
+        customView.backgroundColor = UIColor(colorLiteralRed: 246.0 / 255.0, green: 246.0/255.0, blue: 247.0/255.0, alpha: 1);
+        self.view.addSubview(customView);
+        
+        for var i=0;i<self.viewControllers?.count;i++
+        {
+            
+            let button = UIButton();
+          
+            
+            let width  = customView.frame.size.width / CGFloat((self.viewControllers?.count)!);
+            
+            let x = CGFloat(i) * width;
+            
+            button.frame = CGRectMake(x,0,width,customView.frame.size.height);
+            
+            let image = UIImage(named: "Tabar\(i)")?.imageWithRenderingMode(UIImageRenderingMode.AlwaysOriginal)
+            let unImage = UIImage(named: "UnTabar\(i)")?.imageWithRenderingMode(UIImageRenderingMode.AlwaysOriginal)
+            
+            button.setImage(image, forState: UIControlState.Selected)
+            
+            button.setImage(unImage, forState: UIControlState.Normal)
+            
+            button.adjustsImageWhenHighlighted = false;
+            button.addTarget(self, action: "itemSelect:", forControlEvents: UIControlEvents.TouchUpInside);
+            customView.addSubview(button);
+            button.tag=i;
+            
+            if (i==0)
+            {
+                button.selected = true;
+                self.selectButton = button;
+                
+            }
+            
+        }
+        
+    }
+    func itemSelect(button:UIButton)
+    {
+        self.selectButton.selected = false;
+        button.selected = true;
+        
+        self.selectButton = button;
+        self.selectedIndex = button.tag;
+        
+        
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ StoryBoardDemo2/StoryBoardDemo2/TongXuLuController.swift
@@ -0,0 +1,35 @@
+//
+//  TongXuLuController.swift
+//  StoryBoardDemo2
+//
+//  Created by shimly on 16/5/27.
+//  Copyright © 2016年 shimly. All rights reserved.
+//
+
+import UIKit
+
+class TongXuLuController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.navigationItem.title = "通讯录";
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
